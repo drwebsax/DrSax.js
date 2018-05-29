f3ea2825e4648a1029ab819065b79b5efaa828c8
drwebsax <drwebsax@gmail.com>
2018-05-29 14:25:29 +0900

    change

--- js/drsax.1.8.5.js
+++ js/drsax.1.8.5.js
@@ -18,7 +18,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-(function(window) {
+;(function(window) {
 
     var drsaxContext,
         drsaxInstance,
